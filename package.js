3bd2a0eddb6ab23526d16d65117ed3b41da4525e
Artem Polikarpov <artpolikarpov@gmail.com>
2015-10-25 06:44:40 +0300

    .versions and README.md

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'artpolikarpov:safe-update',
-    version: '1.0.0',
+    version: '1.0.1',
     // Brief, one-line summary of the package.
     summary: 'Make Meteor’s collection.update safer',
     // URL to the Git repository containing the source code for this package.
